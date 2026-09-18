fb879b8045605bbce5c6dd6722c6feb539c93957
jsagitariom619 <jsagitariom619@gmail.com>
2026-09-18 00:10:55 -0400

    Actualizar caché PWA para contenido de salud auditiva

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mi-diario-de-luz-v4';
+const CACHE_NAME = 'mi-diario-de-luz-v5';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', event => {

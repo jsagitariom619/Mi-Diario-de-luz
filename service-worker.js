250b527eeddf676b924fa2908444dd4fafdfe26a
jsagitariom619 <jsagitariom619@gmail.com>
2026-09-18 00:10:00 -0400

    Actualizar caché PWA para módulo de salud

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mi-diario-de-luz-v3';
+const CACHE_NAME = 'mi-diario-de-luz-v4';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', event => {

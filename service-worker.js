aa14f8ebda03832f39077cc0af6175acf2f4e36b
jsagitariom619 <jsagitariom619@gmail.com>
2026-09-17 23:59:26 -0400

    Actualizar caché PWA para nueva rutina diaria

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mi-diario-de-luz-v1';
+const CACHE_NAME = 'mi-diario-de-luz-v2';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', event => {
